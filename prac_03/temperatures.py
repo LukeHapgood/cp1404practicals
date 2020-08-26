3bf10067ad41a1cd339f7ec443ae28ce2ccb345f
Luke Hapgood <luke.hapgood@my.jcu.edu.au>
2020-08-26 16:19:57 +1000

    Correct forrmatting

--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -1,34 +1,37 @@
 """Converts celsius to fahrenheit and vice versa"""
+
+
 def main():
 
-    MENU = """C - Convert Celsius to Fahrenheit
+    menu = """C - Convert Celsius to Fahrenheit
     F - Convert Fahrenheit to Celsius
     Q - Quit"""
-    print(MENU)
+    print(menu)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "C":
             celsius = float(input("Celsius: "))
-            fahrenheit = ceclius_to_fahenheit(celsius)
+            fahrenheit = ceslius_to_farhenheit(celsius)
             print("Result: {:.2f} F".format(fahrenheit))
         elif choice == "F":
-            # Hint: celsius = 5 / 9 * (fahrenheit - 32)
-            # Remove the "pass" statement when you are done. It's a placeholder.
             fahrenheit = float(input("Fahrenheit: "))
             celsius = fahrenheit_to_celsius(fahrenheit)
             print("Result: {:.2f} C".format(celsius))
         else:
             print("Invalid option")
-        print(MENU)
+        print(menu)
         choice = input(">>> ").upper()
     print("Thank you.")
 
-def ceclius_to_fahenheit(celsius):
+
+def ceslius_to_farhenheit(celsius):
     fahrenheit = celsius * 9.0 / 5 + 32
     return fahrenheit
+
 
 def fahrenheit_to_celsius(fahrenheit):
     celsius = 5 / 9 * (fahrenheit - 32)
     return celsius
+
 
 main()
