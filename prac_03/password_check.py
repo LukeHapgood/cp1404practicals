dcdd512147cd41d5b38ea109e0e7b7eee205a93d
Luke Hapgood <luke.hapgood@my.jcu.edu.au>
2020-08-26 14:08:23 +1000

    Refactor password check program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,14 +1,28 @@
 
 min_length = 4
-valid = False
 
-print("Your password must exceed {} characters".format(min_length))
-password = input("Please enter a valid password: ")
+def main():
 
-while not valid:
-    if len(password) >= min_length:
-        print("*"*len(password))
-        valid = True
-    else:
-        print("Invalid password!")
-        password = input("Please enter a valid password: ")
+    password = get_password(min_length)
+
+    hidden_password(password)
+
+
+def hidden_password(password):
+    print("*" * len(password))
+
+
+def get_password(min_length):
+    valid = False
+    print("Your password must contain at least {} characters".format(min_length))
+    password = input("Please enter a valid password: ")
+    while not valid:
+        if len(password) >= min_length:
+            valid = True
+        else:
+            print("Invalid password!")
+            password = input("Please enter a valid password: ")
+    return password
+
+
+main()
