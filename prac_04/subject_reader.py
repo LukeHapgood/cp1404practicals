60941d7644e4e23bceb88e304c3b9934ebbfa394
Luke Hapgood <luke.hapgood@my.jcu.edu.au>
2020-09-02 18:11:21 +1000

    Edit code to return the data rather than printing it
    Add functionality that prints the data neatly

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -10,20 +10,21 @@ def main():
     data = get_data()
     print(data)
 
+    for line in data:
+        print("{} is taught by {:<12} and has {:>3} students".format(line[0], line[1], line[2]))
+
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    data = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        data.append(parts)
     input_file.close()
+    return data
 
 
 main()
