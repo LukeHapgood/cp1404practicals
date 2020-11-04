ffdeddb53ecceac6e5bb2d49c5cc3e69d66daa43
Luke Hapgood <luke.hapgood@my.jcu.edu.au>
2020-11-05 09:59:38 +1000

    Complete initial taxi test

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,11 @@
+from prac_08.taxi import Taxi
+
+prius_taxi = Taxi('Prius 1', 100, 1.23)
+
+prius_taxi.drive(40)
+print('Current fare is ${}'.format(prius_taxi.get_fare()))
+
+prius_taxi.add_fuel(40)
+prius_taxi.start_fare()
+prius_taxi.drive(100)
+print('Current fare is ${}'.format(prius_taxi.get_fare()))
