318ad22b279137b749cc8d3a6fe672f186b549c0
Luke Hapgood <luke.hapgood@my.jcu.edu.au>
2020-11-05 13:30:25 +1000

    Update formatting

--- prac_08/unreliable_car.py
+++ prac_08/unreliable_car.py
@@ -5,22 +5,21 @@ class UnreliableCar(Car):
     """Specialised version of a Car that has added reliability"""
 
     def __init__(self, name, fuel, reliability):
-        """ Initialise an UnreliableCar instance, based on parent class Car.
-
-        name: string, reference name for car
-        fuel: float, one unit of fuel drives one kilometre
-        reliability: a float between 0 and 100, that represents the percentage chance that the drive method will actually work
-        """
-        self.name = name
-        self.fuel = fuel
+        """ Initialise an UnreliableCar instance, based on parent class Car."""
+        super().__init__(name, fuel)
         self.reliability = reliability
-        self.odometer = 0
+
+    def __str__(self):
+        """Return a string like a Car but with current reliability."""
+        return "{}, fuel={}, odometer={}, reliability={}".format(self.name, self.fuel,
+                                                 self.odometer, self.reliability)
 
     def drive(self, distance):
         """Drive the car a given distance.
 
         Drive given distance if car has enough fuel
         or drive until fuel runs out return the distance actually driven.
+        Will not drive if reliability is lower than randomly generated number from 0-100
         """
         reliability_roll = uniform(0, 100)
         if self.reliability > reliability_roll:
