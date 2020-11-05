f87bdc7cd93639605cf5023be40b09e9f90a4f09
Luke Hapgood <luke.hapgood@my.jcu.edu.au>
2020-11-05 22:39:22 +1000

    reformat cleanup_files.py based on solution

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -10,27 +10,21 @@ def main():
     """Demo os module functions."""
     print("Starting directory is: {}".format(os.getcwd()))
 
-    # Change to desired directory
-    os.chdir('Lyrics/Christmas')
+    os.chdir('Lyrics')
+    for directory_name, subdirectories, filenames in os.walk('.'):
+        print("Directory:", directory_name)
+        print("\tcontains subdirectories:", subdirectories)
+        print("\tand files:", filenames)
+        print("(Current working directory is: {})".format(os.getcwd()))
 
-    # Print a list of all files in current directory
-    print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
+        for file in filenames:
+            new_name = get_fixed_filename(filename)
+            print("Renaming {} to {}".format(filename, new_name))
 
-    # Make a new directory
+            source = os.path.join(directory_name, file)
+            new_name = os.path.join(directory_name, get_fixed_filename(file))
+            os.rename(source, new_name)
 
-    try:
-        os.mkdir('temp')
-    except FileExistsError:
-        pass
-
-    # Loop through each file in the (current) directory
-    for filename in os.listdir('.'):
-        # Ignore directories, just process files
-        if os.path.isdir(filename):
-            continue
-
-        new_name = get_fixed_filename(filename)
-        print("Renaming {} to {}".format(filename, new_name))
 
 
 def get_fixed_filename(filename):
@@ -41,19 +35,9 @@ def get_fixed_filename(filename):
 
 def demo_walk():
     """Process all subdirectories using os.walk()."""
-    os.chdir('Lyrics')
-    for directory_name, subdirectories, filenames in os.walk('.'):
-        print("Directory:", directory_name)
-        print("\tcontains subdirectories:", subdirectories)
-        print("\tand files:", filenames)
-        print("(Current working directory is: {})".format(os.getcwd()))
-
-        for file in filenames:
-            source = os.path.join(directory_name, file)
-            new_name = os.path.join(directory_name, get_fixed_filename(file))
-            os.rename(source, new_name)
 
 
 
-# main()
-demo_walk()
+
+main()
+# demo_walk()
